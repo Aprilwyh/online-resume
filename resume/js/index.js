04b934dbf48c03f46d347c87099f012238c86ff4
Aprilwyh <1416402295@qq.com>
2019-06-17 12:25:36 +0800

    [*]新增了部分样式及功能

--- resume/js/index.js
+++ resume/js/index.js
@@ -19,3 +19,38 @@ for(var i=0; i<as.length; i++) {
         divs[index].style.display = "block";
     };
 }
+
+//project
+var project = document.querySelector(".project");
+console.log(project);
+var btn1 = project.getElementsByTagName("button")[0];
+var btn2 = project.getElementsByTagName("button")[1];
+var div1 = project.getElementsByTagName("div")[0];
+var div2 = project.getElementsByTagName("div")[1];
+var flag = false;
+
+btn1.onclick = clickBtn1;
+function clickBtn1() {
+    if(div1.style.opacity == 0 && flag == false) {
+        btn1.style.left = 50+"px";
+        div1.style.opacity = 1;
+        flag = true;
+    }else if(div1.style.opacity == 1 && flag == true) {
+        btn1.style.left = 300+"px";
+        div1.style.opacity = 0;
+        flag = false;
+        btn2.onclick = clickBtn2;
+    }
+};
+btn2.onclick = clickBtn2;
+function clickBtn2() {
+    if(div2.style.opacity == 0 && flag == false) {
+        btn2.style.right = 50+"px";
+        div2.style.opacity = 1;
+        flag = true;
+    }else if(div2.style.opacity == 1 && flag == true) {
+        btn2.style.right = 300+"px";
+        div2.style.opacity = 0;
+        flag = false;
+    }
+};
